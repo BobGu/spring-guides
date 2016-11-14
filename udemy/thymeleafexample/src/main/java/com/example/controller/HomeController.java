066b274a2bd33f4ebd4cdc61140b99be29b42351
Robert Gu <robertgu84@gmail.com>
2016-11-14 14:41:42 -0800

    Person custom exception handling, returning a view

--- udemy/thymeleafexample/src/main/java/com/example/controller/HomeController.java
+++ udemy/thymeleafexample/src/main/java/com/example/controller/HomeController.java
@@ -3,8 +3,11 @@ package com.example.controller;
 import com.example.pojo.People;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +18,19 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("people", createPeople());
         return "home";
+    }
+
+    @RequestMapping("/get/{slug}")
+    public String getPerson(@PathVariable(value="slug") String slug) throws Exception {
+        People people = null;
+        if( people == null ) throw new Exception("We couldn't find the post with slug" + slug);
+        return "people";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(HttpServletRequest req, Exception exception, Model model) {
+        model.addAttribute("errorMessage", exception.getMessage());
+        return "peopleError";
     }
 
     private List<People> createPeople() {
