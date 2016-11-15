5efa0e9db88947eac29eed1d505c94e4705d0a8e
Robert Gu <robertgu84@gmail.com>
2016-11-15 10:03:33 -0800

    Access to h2-console db

---
+++ udemy/src/main/java/com/example/H2InMemoryDbApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2InMemoryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2InMemoryDbApplication.class, args);
+	}
+}
